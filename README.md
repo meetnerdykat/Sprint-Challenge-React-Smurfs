7d2d04b2283fc25867c1c16a54c783537f96cf87
Kathy Cuevas <meetnerdykat@gmail.com>
2019-06-17 23:02:39 -0400

    Added implemented get and post requests. Going to add Router now...

--- README.md
+++ README.md
@@ -32,7 +32,6 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 - [ ] Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
 - [ ] Mention three tools we can use to make AJAX requests.
 
-
 ## Project Set Up
 
 Follow these steps to set up and work on your project:
@@ -53,9 +52,10 @@ Follow these steps to set up and work on your project:
 
 Follow these steps for completing your project:
 
-- [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo).
+- [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's Repo).
 - [ ] Add your Project Manager as a Reviewer on the Pull-request
-- [ ] PM then will count the HW as done by  merging the branch back into master.
+- [ ] PM then will count the HW as done by merging the branch back into master.
+
 ## Minimum Viable Product
 
 The MVP of this project will be broken up between 2 stages. Follow each step and be sure to use your design/style skills to make this application look professional.
--- village/src/App.js
+++ village/src/App.js
@@ -4,21 +4,43 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+import axios from 'axios';
+import { timingSafeEqual } from 'crypto';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  addSmurf = smurf => {
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => this.setState({ smurfs: res.data }));
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm addSmurf={this.addSmurf} />
       </div>
     );
   }
